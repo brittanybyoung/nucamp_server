b427a9797501ce13146fae3754757875604999fe
“Brittany <brittany@signaturegroupevents.com>
2025-05-15 11:00:24 -0400

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
+const mongoose = require('mongoose')
+
+const url = 'mongodb://127.0.0.1:27017/nucampsite';
+const connect = mongoose.connect(url, {});
+
+connect.then(() => console.log('Connect correctly to server'),
+  err  => console.log(err)
+);
 
 var app = express();
 
