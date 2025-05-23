d4eb668fcab2bc0ce8a7b6be562be67eb59aa0c8
“Brittany <brittany@signaturegroupevents.com>
2025-05-23 10:13:29 -0400

    Week 3 Workshop

--- routes/users.js
+++ routes/users.js
@@ -7,35 +7,42 @@ const router = express.Router();
 
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+router.route('/')
+  .get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Users.find({})
+      .then(users => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users)
+      })
+      .catch(err => next(err))
+  });
 
 router.post('/signup', (req, res) => {
   const user = new User({ username: req.body.username });
 
   User.register(user, req.body.password)
     .then(registeredUser => {
-    if (req.body.firstname) {
-      registeredUser.firstname = req.body.firstname;
-    }
-    if (req.body.lastname) {
-      registeredUser.lastname = req.body.lastname;
-    }
-    return registeredUser.save();
-  })
-  .then(() => {
-    passport.authenticate('local')(req, res, () => {
-      res.statusCode = 200;
+      if (req.body.firstname) {
+        registeredUser.firstname = req.body.firstname;
+      }
+      if (req.body.lastname) {
+        registeredUser.lastname = req.body.lastname;
+      }
+      return registeredUser.save();
+    })
+    .then(() => {
+      passport.authenticate('local')(req, res, () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ success: true, status: 'Registration Successful!' });
+      });
+    })
+    .catch(err => {
+      res.statusCode = 500;
       res.setHeader('Content-Type', 'application/json');
-      res.json({ success: true, status: 'Registration Successful!' });
-    });
-  })
-  .catch(err => {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({ err: err });
-  })
+      res.json({ err: err });
+    })
 });
 
 router.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
